5479248b2565673bf5e11d328427278c23e760df
EvaReboucas30 <140523579+EvaReboucas30@users.noreply.github.com>
2025-09-17 07:59:10 -0300

    projeto freebie

--- freebiepanel/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from django.contrib import admin
-
-urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),  # 👈 ADICIONE ESSA LINHA
-    path('admin/', admin.site.urls),
-    # outras urls...
-]
